1fc6ee791b4dfc71799ce7550856989eb4341633
SomeoneHoi <47040996+SomeoneHoi@users.noreply.github.com>
2021-02-01 19:32:41 +0100

    Added code to enable debugging of the editor
    Empty code to create project files
    editor can be debugged through an IDE
    updated ignore file to filter Rider temp folder

---
+++ Source/GGJ21/EmptyClassForEditorDebugging.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EmptyClassForEditorDebugging.h"
+
+EmptyClassForEditorDebugging::EmptyClassForEditorDebugging()
+{
+}
+
+EmptyClassForEditorDebugging::~EmptyClassForEditorDebugging()
+{
+}
